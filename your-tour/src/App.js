e4c153bf812621345b9d9a7faa4b4e40a643cfd0
Maria <17mashmallow@gmail.com>
2023-02-28 21:11:03 +0300

    add new filder for the ChooseTour component + resolve problem with path of the card photos

--- your-tour/src/App.js
+++ your-tour/src/App.js
@@ -1,6 +1,7 @@
 import './styles/AppStyles/App.css';
 
 import Header from './components/Header/Header';
+import ChooseTour from './components/ChooseTourSection/ChooseTour/ChooseTour';
 import MailSection from './components/Mail-Section/MailSection';
 import Footer from './components/Footer/Footer';
 
@@ -8,6 +9,7 @@ function App() {
   return (
     <div className="wrapper">
       <Header />
+      <ChooseTour />
       <MailSection />
       <Footer />
     </div>
--- your-tour/src/components/DetailsBtn/DetailsBtn.jsx
+++ your-tour/src/components/DetailsBtn/DetailsBtn.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import './DetailsBtn.css';
 import arrow from './DetailsBtnPng/Vector.png';
 
-export default function DetailsBtn() {
+export default function DetailsBtn(additionalClass) {
   return (
-    <div className="info-link__container">
+    <div className={`info-link__container ${additionalClass}`}>
       <p className="info-link__text">Подробнее</p>
       <div className="info-link__arrow">
         <img src={arrow} alt="arrow" />
